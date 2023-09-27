bfb859d8bc876c826e1ddc0fa1a52934c35a3b19
jueun <juju1004juju@gmail.com>
2023-09-27 17:13:50 +0900

    feat: header 반응형 각 페이지에 적용

--- js/detail.js
+++ js/detail.js
@@ -10,5 +10,11 @@ window.addEventListener("load", function () {
         },
         });
         AOS.init();
-        
+        // 헤더
+        const menuToggleBtn = document.getElementById("menuToggleBtn");
+        const mobileMenubox = document.getElementById("mobile-menu-box");
+        menuToggleBtn.addEventListener("click", function () {
+        // Toggle the 'active' class to show/hide the mobile menu
+        mobileMenubox.classList.toggle("active");
+        });
 });
--- js/reservation.js
+++ js/reservation.js
@@ -179,4 +179,11 @@ window.addEventListener("load", function () {
       reservationContent.style.display = "block";
     }
   }
+  // 헤더
+  const menuToggleBtn = document.getElementById("menuToggleBtn");
+  const mobileMenubox = document.getElementById("mobile-menu-box");
+  menuToggleBtn.addEventListener("click", function () {
+  // Toggle the 'active' class to show/hide the mobile menu
+  mobileMenubox.classList.toggle("active");
+  });
 });
